df6d77dd32aaa5a8b2ba572fb681bba11d9f83d5
gcl1991 <737720583@qq.com>
2020-11-21 16:49:06 +0800

    学习大对象直接进入老年代

--- java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter3/SerialGCAllocation.java
+++ java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter3/SerialGCAllocation.java
@@ -1,5 +1,8 @@
 package org.fenixsoft.jvm.chapter3;
 
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
 /**
  * VM参数：-XX:+UseSerialGC
  * @author zzm
@@ -8,25 +11,28 @@ public class SerialGCAllocation {
 
     private static final int _1MB = 1024 * 1024;
 
+    public static void main(String[] args) {
+        testPretenureSizeThreshold();
+    }
     /**
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
      */
     public static void testAllocation() {
         byte[] allocation1, allocation2, allocation3;
-        allocation1 = new byte[_1MB];
-        allocation2 = new byte[_1MB];
-        allocation3 = new byte[_1MB]; // 出现一次Minor GC
+        allocation1 = new byte[2*_1MB];
+        allocation2 = new byte[2*_1MB];
+        allocation3 = new byte[2*_1MB]; // 出现一次Minor GC
     }
-//
-//    /**
-//     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
-//     * -XX:PretenureSizeThreshold=3145728
-//     */
-//    public static void testPretenureSizeThreshold() {
-//        byte[] allocation;
-//        allocation = new byte[4 * _1MB];  //直接分配在老年代中
-//    }
-//
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * -XX:PretenureSizeThreshold=3145728
+     */
+    public static void testPretenureSizeThreshold() {
+        byte[] allocation;
+        allocation = new byte[4 * _1MB];  //直接分配在老年代中
+    }
+
 //    /**
 //     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1
 //     * -XX:+PrintTenuringDistribution
--- java/deep-thinking-jvm-3/src/test/java/org/fenixsoft/jvm/chapter3/SerialGCAllocationTest.java
+++ java/deep-thinking-jvm-3/src/test/java/org/fenixsoft/jvm/chapter3/SerialGCAllocationTest.java
@@ -3,10 +3,10 @@ package org.fenixsoft.jvm.chapter3;
 import org.junit.Test;
 
 public class SerialGCAllocationTest {
-    @Test
-    public void testAllocation(){
-        SerialGCAllocation.testAllocation();
-    }
+//    @Test
+//    public void testAllocation(){
+//        SerialGCAllocation.testAllocation();
+//    }
 
 
 }
