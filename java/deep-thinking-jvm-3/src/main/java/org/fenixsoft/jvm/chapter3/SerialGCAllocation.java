b263d3e9814a556961cfee61bb9cd372e1ea38c4
gcl1991 <737720583@qq.com>
2020-11-22 11:53:48 +0800

    学习长期存活对象进入老年区

--- java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter3/SerialGCAllocation.java
+++ java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter3/SerialGCAllocation.java
@@ -12,7 +12,7 @@ public class SerialGCAllocation {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) {
-        testPretenureSizeThreshold();
+        testTenuringThreshold();
     }
     /**
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
@@ -33,20 +33,18 @@ public class SerialGCAllocation {
         allocation = new byte[4 * _1MB];  //直接分配在老年代中
     }
 
-//    /**
-//     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1
-//     * -XX:+PrintTenuringDistribution
-//     */
-//    public static void testTenuringThreshold() {
-//        byte[] allocation1, allocation2, allocation3;
-//        allocation1 = new byte[_1MB / 4];  // 什么时候进入老年代决定于XX:MaxTenuringThreshold设置
-//        allocation2 = new byte[4 * _1MB];
-//        allocation3 = new byte[4 * _1MB];
-//        allocation3 = null;
-//        allocation3 = new byte[4 * _1MB];
-//    }
-//
-//    /**
+    /**
+     * VM参数：-verbose:gc -Xms30M -Xmx30M -Xmn15M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1
+     * -XX:+PrintTenuringDistribution 分配
+     */
+    public static void testTenuringThreshold() {
+        byte[] allocation1;
+        allocation1 = new byte[10*_1MB];
+        allocation1 = null;
+        allocation1 = new byte[10*_1MB];
+    }
+
+    /**
 //     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
 //     * -XX:+PrintTenuringDistribution
 //     */
