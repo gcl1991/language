d4befd9a328869d9366aecb2602044821a9fc236
gcl1991 <737720583@qq.com>
2020-11-22 12:17:24 +0800

    学习动态年龄和分配担保

--- java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter3/SerialGCAllocation.java
+++ java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter3/SerialGCAllocation.java
@@ -12,7 +12,7 @@ public class SerialGCAllocation {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) {
-        testTenuringThreshold();
+        testHandlePromotion();
     }
     /**
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
@@ -45,36 +45,36 @@ public class SerialGCAllocation {
     }
 
     /**
-//     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
-//     * -XX:+PrintTenuringDistribution
-//     */
-//    public static void testTenuringThreshold2() {
-//        byte[] allocation1, allocation2, allocation3, allocation4;
-//        allocation1 = new byte[_1MB / 4];   // allocation1+allocation2大于survivo空间一半
-//        allocation2 = new byte[_1MB / 4];
-//        allocation3 = new byte[4 * _1MB];
-//        allocation4 = new byte[4 * _1MB];
-//        allocation4 = null;
-//        allocation4 = new byte[4 * _1MB];
-//    }
-//
-//    /**
-//     * VM参数：-Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:-HandlePromotionFailure
-//     */
-//    public static void testHandlePromotion() {
-//        byte[] allocation1, allocation2, allocation3, allocation4, allocation5, allocation6, allocation7;
-//        allocation1 = new byte[2 * _1MB];
-//        allocation2 = new byte[2 * _1MB];
-//        allocation3 = new byte[2 * _1MB];
-//        allocation1 = null;
-//        allocation4 = new byte[2 * _1MB];
-//        allocation5 = new byte[2 * _1MB];
-//        allocation6 = new byte[2 * _1MB];
-//        allocation4 = null;
-//        allocation5 = null;
-//        allocation6 = null;
-//        allocation7 = new byte[2 * _1MB];
-//    }
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
+     * -XX:+PrintTenuringDistribution
+     */
+    public static void testTenuringThreshold2() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[_1MB / 4];   // allocation1+allocation2大于survivo空间一半
+        allocation2 = new byte[_1MB / 4];
+        allocation3 = new byte[4 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+        allocation4 = null;
+        allocation4 = new byte[4 * _1MB];
+    }
+
+    /**
+     * VM参数：-Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:-HandlePromotionFailure
+     */
+    public static void testHandlePromotion() {
+        byte[] allocation1, allocation2, allocation3, allocation4, allocation5, allocation6, allocation7;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation1 = null;
+        allocation4 = new byte[2 * _1MB];
+        allocation5 = new byte[2 * _1MB];
+        allocation6 = new byte[2 * _1MB];
+        allocation4 = null;
+        allocation5 = null;
+        allocation6 = null;
+        allocation7 = new byte[2 * _1MB];
+    }
 
 
 
