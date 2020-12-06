1e90e304b285b1b9715e13f34de9826deef27a3b
gcl1991 <737720583@qq.com>
2020-12-06 14:35:36 +0800

    清理，删除无用文件

--- java/research/array/Demo04.java
+++
@@ -1,9 +0,0 @@
-package learn_java.array;
-
-public class Demo04 {
-    public static void main(String[] args) {
-        String[] months = {"1","2","3","4","5","6","7","8","9","10","11","12"};
-        int i = 1;
-        System.out.println(months[i]);
-    }
-}
--- java/research/variable/VariableDemo03.java
+++
@@ -1,7 +0,0 @@
-package learn_java.variable;
-
-public class VariableDemo03 {
-    public static void main(String[] args) {
-        final int x = 3;
-    }
-}
