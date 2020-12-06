4e0233c6bbe552411a5efbd30e47054feb5559c5
gcl1991 <737720583@qq.com>
2020-12-06 19:02:42 +0800

    查看即时编译结果

--- java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter11/DropLoop.java
+++ java/deep-thinking-jvm-3/src/main/java/org/fenixsoft/jvm/chapter11/DropLoop.java
@@ -2,11 +2,9 @@ package org.fenixsoft.jvm.chapter11;
 
 public class DropLoop {
     public static final int NUM = 15000;
-
-    public static void main(String[] args) {
-        for (int i = 0; i < NUM; i++) {
-            calcSum();
-        }
+    public static int doubleValue(int i) {
+        for (int j = 0; j < 100000; j++);
+        return i * 2;
     }
 
     public static long calcSum() {
@@ -17,9 +15,10 @@ public class DropLoop {
         return sum;
     }
 
-    public static int doubleValue(int i) {
-        for (int j = 0; j < 100000; j++) {
+    public static void main(String[] args) {
+        for (int i = 0; i < NUM; i++) {
+            calcSum();
         }
-        return i * 2;
     }
+
 }
